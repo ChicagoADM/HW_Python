4db0033aefcf92b74323185e9eb3d3180e6733eb
4icag0 <112687685+ChicagoADM@users.noreply.github.com>
2023-07-24 20:45:58 +0300

    Update Task_03.py

--- HW_05/Task_03.py
+++ HW_05/Task_03.py
@@ -1,11 +1,11 @@
 # ✔Создайте функцию генератор чисел Фибоначчи 
 
-figure = int(input("Введите количество цифр: "))
+numbers = int(input("Введите количество чисел для генерации чисел Фибоначчи: "))
 def get_fibonacci(num: int):
     f1, f2 = 0, 1
     for _ in range(num):
         yield f2
         f1, f2 = f2, f1 + f2
 
-for i in get_fibonacci(figure):
+for i in get_fibonacci(numbers):
     print(i, end=', ')
